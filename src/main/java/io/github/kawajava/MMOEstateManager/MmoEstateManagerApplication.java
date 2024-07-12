202a303200149c9dac624ab2ca65d47f780876e3
kawajava <kawajava0550@gmail.com>
2024-07-12 14:53:32 +0200

    initial commit

---
+++ src/main/java/io/github/kawajava/MMOEstateManager/MmoEstateManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.kawajava.MMOEstateManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoEstateManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoEstateManagerApplication.class, args);
+	}
+
+}
