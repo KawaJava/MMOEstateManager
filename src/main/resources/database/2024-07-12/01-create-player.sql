26d461eee48d20c48a2e6f94dd520aac91b927a3
kawajava <kawajava0550@gmail.com>
2024-07-12 19:04:20 +0200

    db dependencies added and column player creaded

---
+++ src/main/resources/database/2024-07-12/01-create-player.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset kawajava:1
+create table player (
+    id bigint not null auto_increment PRIMARY KEY,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    clan varchar(255) not null,
+    isActive boolean default true not null,
+    created date not null
+);
