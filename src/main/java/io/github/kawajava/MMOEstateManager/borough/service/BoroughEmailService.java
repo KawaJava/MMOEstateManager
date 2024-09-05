b68b1bda165e184bfca4052458f0e2942d89d88e
kawajava <kawajava0550@gmail.com>
2024-09-05 12:25:11 +0200

    BoroughEmailService fetching players added

--- src/main/java/io/github/kawajava/MMOEstateManager/borough/service/BoroughEmailService.java
+++ src/main/java/io/github/kawajava/MMOEstateManager/borough/service/BoroughEmailService.java
@@ -2,9 +2,10 @@ package io.github.kawajava.MMOEstateManager.borough.service;
 
 import io.github.kawajava.MMOEstateManager.borough.model.Borough;
 import io.github.kawajava.MMOEstateManager.common.repository.BoroughRepository;
+import io.github.kawajava.MMOEstateManager.common.repository.PlayerRepository;
+import io.github.kawajava.MMOEstateManager.player.model.Player;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,25 +15,25 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class BoroughEmailService {
 
     @Value("${time.to.add.updated.gold}")
     private int daysToUpdateGold;
 
     private final BoroughRepository boroughRepository;
+    private final PlayerRepository playerRepository;
 
     @Transactional
     @Scheduled(cron = "${cron.borough.time.email.expression}")
     public void updateBoroughsWhereGoldIsAddedTooLate() {
         List<Borough> boroughs = boroughRepository.findByEmailSendFalseAndDateAddedLessThan
                 (LocalDateTime.now().minusDays(daysToUpdateGold));
-        log.info("gminy z przekroczonym czasem dodania złota " + boroughs.size());
 
         List<Long> playersIds = boroughs.stream()
                 .map(Borough::getActualLeaderId)
                 .distinct()
                 .toList();
+        List<Player> players = playerRepository.selectAllByIdIn(playersIds);
 
         //emails to players
 
