04d988e8ce4554365f215e7293da58d9a18d6a1f
kawajava <kawajava0550@gmail.com>
2024-09-07 18:10:32 +0200

    Code refactored: Emails packages and properties improved

--- src/main/java/io/github/kawajava/MMOEstateManager/mailing/config/EmailConfig.java
+++ src/main/java/io/github/kawajava/MMOEstateManager/mailing/config/EmailConfig.java
@@ -1,5 +1,8 @@
-package io.github.kawajava.MMOEstateManager.mailing.service;
+package io.github.kawajava.MMOEstateManager.mailing.config;
 
+import io.github.kawajava.MMOEstateManager.mailing.service.EmailSender;
+import io.github.kawajava.MMOEstateManager.mailing.service.EmailStandardService;
+import io.github.kawajava.MMOEstateManager.mailing.service.FakeEmailService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
